140ab239658a0f9ee9e541d2b65f08801a06a3f3
lantgabor <lant.gabor@icloud.com>
2025-06-23 21:09:29 +0200

    init react vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-/**
- * @type {import('vite').UserConfig}
- *
- */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default {
-  base: process.env.NODE_ENV === "production" ? "/prog-learn/" : "/",
-};
+export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/prog-learn" : "/",
+  plugins: [react()],
+});
