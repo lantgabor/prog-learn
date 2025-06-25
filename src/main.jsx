0812d73cf7e1e0a0d8b37c934e7e52d2e924be99
lantgabor <lant.gabor@icloud.com>
2025-06-25 12:41:46 +0200

    adding rehype + remark plugins

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Hello from "./Hello.mdx";
+import Gfm from "./Gfm.mdx";
+import MMath from "./MMath.mdx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <Hello />
+    <Gfm />
+    <MMath />
   </StrictMode>,
-)
+);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import mdx from "@mdx-js/rollup";
+
+import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
+
+import rehypeStarryNight from "rehype-starry-night";
+import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/prog-learn" : "/",
-  plugins: [react()],
+  plugins: [
+    {
+      enforce: "pre",
+      ...mdx({
+        remarkPlugins: [remarkGfm, remarkMath],
+        rehypePlugins: [rehypeStarryNight, rehypeKatex],
+        /* jsxImportSource: …, otherOptions… */
+      }),
+    },
+    react(),
+  ],
 });
